f2427c11a12f249765ebd56ec6d764e1fd1e2000
王亚飞Ricky <ricky.wang@qunarservers.com>
2017-06-18 22:39:20 +0800

    webpack

---
+++ client/index.js
@@ -0,0 +1,8 @@
+console.log('dandan');
+console.log('dapangzi');
+console.log('love');
+
+var moment = require('moment');
+var $ = require('jquery');
+console.log(moment().format());
+console.log($('#app'))
---
+++ webpack.base.config.js
@@ -0,0 +1,38 @@
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var path = require('path');
+
+module.exports = {
+	devtool:'eval-source-map',
+	entry:{
+		main:path.join(__dirname,'./client/index.js'),
+		vendor:'moment'
+	},
+	output:{
+		path:path.join(__dirname,'./dist'),
+		filename:"[name][hash].js"
+	},
+	module:{
+		rules:[
+			{
+				test:/\.css$/,
+				use:['style-loader','css-loader']
+			}
+		]
+	},
+	plugins:[
+		//压缩混淆代码
+		new webpack.optimize.UglifyJsPlugin(),
+		//编译html
+		new HtmlWebpackPlugin({template:'./index.html'}),
+		//分块打包
+		new webpack.optimize.CommonsChunkPlugin({
+            name: 'vendor',
+            minChunks: function (module) {
+               // 该配置假定你引入的 vendor 存在于 node_modules 目录中
+               return module.context && module.context.indexOf('node_modules') !== -1;
+            }
+        })
+	]
+};
