c0cbd05952ea921324337c38405ce2fb0b431488
李丹丹 <lidandan@lidandandeMacBook-Pro.local>
2017-07-12 00:25:08 +0800

    markdown

--- client/route/routes.js
+++ client/route/routes.js
@@ -2,6 +2,9 @@ import App from '../App.vue'
 import Index from '../views/user/Index.vue'
 import Blog from '../views/user/Blog.vue'
 
+import AdminIndex from '../views/admin/Index.vue'
+import Add from '../views/admin/Add.vue'
+
 export default [
  	{ 
  		path: '/',
@@ -13,6 +16,14 @@ export default [
 	    children: [{
 	        path: 'blog',
 	        component: Blog
+	    }]
+	},
+	{
+	    path: '/admin',
+	    component: AdminIndex,
+	    children: [{
+	        path: 'add',
+	        component: Add
 	    }]
 	}
 ]
---
+++ client/views/admin/Add.vue
@@ -0,0 +1,81 @@
+<template>
+	<div class="admin-add">
+        <div class="publish">
+            <a class="publish-btn">发表博客</a>
+        </div>
+        <div class="markdownContainer">
+            <div class="left">
+                <textarea  placeholder="markdown..."  v-model="mdContent"></textarea>
+            </div>
+            <div class="right" v-html="mdHtml">
+            </div> 
+        </div>
+		
+	</div>
+</template>
+<script>
+	import marked  from 'marked'
+    marked.setOptions({
+        renderer: new marked.Renderer(),
+        gfm: true,
+        tables: true,
+        breaks: false,
+        pedantic: false,
+        sanitize: false,
+        smartLists: true,
+        smartypants: false
+    });
+	export default {
+		data(){
+			return {
+                mdContent:''
+			};
+		},
+		created(){
+		},
+        computed:{
+            mdHtml(){
+                return marked(this.mdContent,{sanitize:true})||'预览...';
+            }
+        },
+        methods:{
+        }
+	}
+
+</script>
+<style scoped lang="less">
+	.admin-add{
+		margin:20px;
+        .publish{
+            height:60px;
+            .publish-btn{
+                background:#7EC0EE;
+                height:30px;
+                padding:15px 15px;
+                color:#fff;
+                border-radius:5px;
+                cursor:pointer;
+                &:hover{
+                    background:#337ab7;
+                }
+            }
+        }
+        .markdownContainer{
+            display:flex;
+            minheight:640px;
+            .left{
+                width:48%;
+                margin-right:20px;
+                textarea{
+                    height:640px;
+                    width:100%;
+                }
+            }
+            .right{
+                width:48%;
+                border:solid 1px rgba(0,0,0,0.4);
+                overflow:auto;
+            }
+        }
+	}
+</style>
